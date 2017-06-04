311f7365bde807166990f0a604752a0ba190569f
mathieuHa <mathieu.hanotaux@gmail.com>
2017-06-04 13:02:05 +0200

    add structure of the Website

--- src/DTRESiteBundle/Controller/DefaultController.php
+++ src/DTRESiteBundle/Controller/DefaultController.php
@@ -10,4 +10,30 @@ class DefaultController extends Controller
     {
         return $this->render('DTRESiteBundle:Default:index.html.twig');
     }
+
+    public function teamAction()
+    {
+        return $this->render('DTRESiteBundle:Default:team.html.twig');
+    }
+
+    public function projectAction()
+    {
+        return $this->render('DTRESiteBundle:Default:project.html.twig');
+    }
+
+    public function eventAction()
+    {
+        return $this->render('DTRESiteBundle:Default:event.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('DTRESiteBundle:Default:contact.html.twig');
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('DTRESiteBundle:Default:about.html.twig');
+    }
+
 }
